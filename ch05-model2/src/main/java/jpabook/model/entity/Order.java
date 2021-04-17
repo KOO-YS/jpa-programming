711c4250f96fe1761faf901a619820e0e9fff526
yaans <kbeeysk@naver.com>
2021-04-17 15:40:45 +0900

    [chapter 05] (실습 예제) 양방향 연관관계 매핑

---
+++ ch05-model2/src/main/java/jpabook/model/entity/Order.java
@@ -0,0 +1,82 @@
+package jpabook.model.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    // 연관관계의 주인 (MEMBER <-> ORDER)
+    @ManyToOne                          
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;      // 주문 회원
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;     //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;//주문상태
+
+    // 연관관계 설정
+    public void setMember(Member member) {
+        //기존 연관관계 삭제
+        if (this.member != null) {
+            this.member.getOrders().remove(this);
+        }
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+
+}
